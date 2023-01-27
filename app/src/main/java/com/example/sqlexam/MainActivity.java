fbeab68133f909a987daf74e570e722eb447f659
Yaroslav <baeviaroslav@yandex.ru>
2023-01-27 17:06:35 +0300

    reload projectt

--- app/src/main/java/com/example/sqlexam/MainActivity.java
+++ app/src/main/java/com/example/sqlexam/MainActivity.java
@@ -3,12 +3,62 @@ package com.example.sqlexam;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import java.io.BufferedReader;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button btnsignin1;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        btnsignin1 = (Button) findViewById(R.id.btnsignin1);
+        btnsignin1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                URL url = null;
+                try {
+                    url = new URL("https://db67-79-139-155-117.eu.ngrok.io/login");
+                } catch (MalformedURLException e) {
+                    e.printStackTrace();
+                }
+                HttpURLConnection connection = null;
+                try {
+                    connection = (HttpURLConnection) url.openConnection();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+
+                try {
+                    assert connection != null;
+                    BufferedReader br = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+
+                    // read the input stream
+                    // in this case, I simply read the first line of the stream
+                    String line = br.readLine();
+                    Log.d("HTTP-GET", line);
+
+                } catch (IOException e) {
+                    e.printStackTrace();
+                } finally {
+                    connection.disconnect();
+                }
+
+            }
+        });
     }
+
 }
